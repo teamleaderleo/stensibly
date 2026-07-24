5f3b50563aa7f17e553731226e1118a6a1515488
Leo <leoli_082000@hotmail.com>
2026-07-25 02:25:38 +0800

    Revert incomplete dashboard item-detail merge (#44)
    * Revert incomplete dashboard item detail slice
    
    * Remove orphaned item detail helper
    
    * Remove failing item detail contract test
    
    * Remove orphaned item detail regression test
    
    * Remove incomplete item detail helper tests

--- test/dashboard-item-detail-undefined.test.ts
+++
@@ -1,8 +0,0 @@
-import { expect, test } from "bun:test";
-import { payloadEntries } from "../site/item-detail.js";
-
-test("dashboard item detail formats undefined payload values", () => {
-  expect(payloadEntries({ missing: undefined })).toEqual([
-    { key: "missing", value: "undefined" },
-  ]);
-});
