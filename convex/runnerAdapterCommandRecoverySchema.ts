1e2772e3668d02e90bcaf888bda940ee6ca8cc7c
Leo <cheerleaderleo@outlook.com>
2026-08-15 21:47:28 +0800

    Preserve current mail schema packs

--- convex/runnerAdapterCommandRecoverySchema.ts
+++ convex/runnerAdapterCommandRecoverySchema.ts
@@ -1,5 +1,6 @@
 import { defineTable } from "convex/server";
 import { v } from "convex/values";
+import { gmailMailboxDispositionTables } from "./gmailMailboxDispositionSchema";
 import { mailboxIntakeTables } from "./mailboxIntakeSchema";
 import { mailOutboundTables } from "./mailOutboundSchema";
 import { mailSemanticAdmissionTables } from "./mailSemanticAdmissionSchema";
@@ -9,6 +10,7 @@ export const runnerAdapterCommandRecoveryTables = {
   ...mailboxIntakeTables,
   ...mailSemanticAdmissionTables,
   ...mailOutboundTables,
+  ...gmailMailboxDispositionTables,
   ...outlookRuntimeTables,
   runnerAdapterCommandRecoveries: defineTable({
     workspaceId: v.id("workspaces"),
